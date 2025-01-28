63de374e8d23ce1c4531e61daa7a500d7460673b
viveklams <viveklamgadey@gmail.com>
2025-01-28 16:20:47 +0530

    we created a link for my LinkedIN page so that i can go and add my link to my pages here i used an <a> tag for that inside a span tag  and all the designs possible

--- Vlama-portfolio/src/sections/Hero/Hero.jsx
+++ Vlama-portfolio/src/sections/Hero/Hero.jsx
@@ -1,11 +1,39 @@
 import styles from "./HeroStyles.module.css";
 import heroImg from "../../assets/hero-img.png";
+import themeIcon from "../../assets/sun.svg";
+import LinkedInIcon from "../../assets/linkedin-light.svg";
 
 const Hero = () => {
   return (
-    <section id="hero">
-      <div>
-        <img src={heroImg} alt="" />
+    <section id="hero" className={styles.container}>
+      <div className={styles.colorModeContainer}>
+        <img
+          className={styles.hero}
+          src={heroImg}
+          alt="Profile Picture of Vivek Biswakarma"
+        />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color Mode Icon"
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Vivek
+          <br />
+          Biswakarma
+        </h1>
+        <h2>Full Stack Developer</h2>
+        <span>
+          <a
+            href="https://www.linkedin.com/in/vivek-biswakarma-5b850a314/"
+            target="_blank"
+          >
+            {" "}
+            <img src={LinkedInIcon} alt="Linked In Icon" />
+          </a>
+        </span>
       </div>
     </section>
   );
